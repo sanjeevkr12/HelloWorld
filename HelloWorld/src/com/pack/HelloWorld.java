cc56bdd1c19f84e08f1bd58421cf22451399fb49
Sintu <Sintu@192.168.2.11>
2019-01-22 00:27:15 +0530

    dfjdhfd

--- HelloWorld/src/com/pack/HelloWorld.java
+++ HelloWorld/src/com/pack/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		for(int i=0;i<=10;i++) {
-			System.out.println("Hello-> "+i);
+			System.out.println("Hello---> "+i);
 		}
 		for(int i=0;i<=10;i++) {
 			System.out.println("Hello-> "+i);
